56b98445be66bb1532d4b417b194bbc09511fd74
DWilcox02 <danielgwilcox@gmail.com>
2022-05-20 22:06:03 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from song import Song
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from cloudscraper import cloudscraper
+
+# Get artists
+# artist1 = input("Artist 1: ")
+artist1 = "lil peep"
+artist1 = artist1.lower().strip().replace(" ", "-")
+# artist2 = input("Artist 2: ")
+# artist2 = artist2.lower().strip().replace(" ", "-")
+
+r1 = requests.get("https://tunebat.com/Search?q=%s" % (artist1))
+c1 = r1.content
+soup1 = BeautifulSoup(c1, "html.parser")
+
+scraper = cloudscraper.create_scraper()
+r1 = scraper.get("https://tunebat.com/Search?q=%s" % (artist1))
+with open("html.txt", "w") as file:
+    file.write(r1.text())
---
+++ song.py
@@ -0,0 +1,8 @@
+
+
+class Song:
+
+    def __init__(self, name, key, tempo) -> None:
+        self.name = name
+        self.key = key
+        self.tempo = tempo
